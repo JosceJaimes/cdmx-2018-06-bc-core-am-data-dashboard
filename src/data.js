801ac0fa5d510e503f56f6af5a825ad87a9f9a48
Josce Jaimes <nanayhachi94@gmail>
2018-07-27 05:28:27 -0500

    generando cambios en colores en la cabecera y eslint

--- src/data.js
+++ src/data.js
@@ -1,34 +1,114 @@
-window.global = {
-  //Se genera una función y dentro de ella una constante con la url del JSON para llamarlo
-  computeStudentsStats: (laboratoria) => {
-    const url = `https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`;
-    fetch(url)
-      .then(respuesta => respuesta.json())
-      .then(data => {
-        const generaciones = data.lima.generacion;
-        estudiante(data)
-      })
-  }
-};
+const container = document.getElementById('alumnas'); 
+ 
+laboratoria = (data) =>{
+  document.getElementById('boton1').addEventListener('click', (event) => {
+    let estudiante = data.lima.generacion.cuarta.estudiantes;
+    let progress = estudiante.progreso;
+    let result = '';
+    let status = '';
+    for (progress of estudiante) {
+      let pro = progress.progreso.porcentajeCompletado;
+      let percentDuration = progress.progreso.duracionPrograma;
+      //  console.log(percentComplete);
+      if (pro >= 60) {
+        status = 'Alumna promedio';
+      } else if (pro <= 60) {
+        status = 'Alumna con con bajo desempeño';
+      } else if (pro >= 90) {
+        status = 'Alumna sobresaliente';
+      }   
+      
+      for (let i = 0; i < estudiante.length; i++) {
+        let nombres = estudiante[i];
+        // console.log(percentComplete);
+        // // console.log(status);
+        result += `<div class="col-md-4">
+      <div class="card">
+      <div class = "estudiante">
+    
+      <p>Nombre: ${nombres.nombre}</p>
+      <p>Correo: ${nombres.correo}</p>
+      <p>Turno: ${nombres.turno}</p>
+      <p>Estatus: ${status}</p>
+      <p>Duración del programa: ${percentDuration}</p>
+      </div>
+      </div>
+      </div> `;
+      }
+      container.innerHTML = result;
+      return result;
+    };
+  });
 
-estudiante = (data) => {
-  let estudiante = data.lima.generacion.cuarta.estudiantes;
-  let result = "";
-  for (let i = 0; i < estudiante.length; i++) {
-    let nombres = estudiante[i];
-    //console.log(nombres, pintar);
-    result = result + `
-          <tr>
-            <th scope="row"></th>
-            <td>${nombres.correo}</td>
-            <td>${nombres.nombre}</td>
-            <td>${nombres.turno}</td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-`
-  }
-  //Impresión del resultado
-  pintar.innerHTML = result;
-}
+
+  document.getElementById('boton2').addEventListener('click', (event) => {
+    let estudiante = data.santiago.generacion.cuarta.estudiantes;
+    let progress = estudiante.progreso;
+    let result = '';
+    for (progress of estudiante) {
+      let pro = progress.progreso.porcentajeCompletado;
+      let percentDuration = progress.progreso.duracionPrograma;
+      //  console.log(percentComplete);
+      if (pro >= 60) {
+        status = 'Alumna promedio';
+      } else if (pro <= 60) {
+        status = 'Alumna con con bajo desempeño';
+      } else if (pro >= 90) {
+        status = 'Alumna sobresaliente';
+      }   
+      
+      for (let i = 0; i < estudiante.length; i++) {
+        let nombres = estudiante[i];
+        // console.log(nombres);
+        result += `<div class="card">
+        <div class = "estudiante">
+      
+        <p>Nombre: ${nombres.nombre}</p>
+        <p>Correo: ${nombres.correo}</p>
+        <p>Turno: ${nombres.turno}</p>
+        <p>Estatus: ${status}</p>
+        <p>Duración del programa: ${percentDuration}</p>
+        </div>
+        </div> `;
+      }
+      container.innerHTML = result;
+      return result;
+    }
+  }),
+
+
+  document.getElementById('boton3').addEventListener('click', (event) => {
+    let estudiante = data.mexico.generacion.cuarta.estudiantes;
+    let progress = estudiante.progreso;
+    let result = '';
+   
+    for (progress of estudiante) {
+      let pro = progress.progreso.porcentajeCompletado;
+      let percentDuration = progress.progreso.duracionPrograma;
+      //  console.log(percentComplete);
+      if (pro >= 60) {
+        status = 'Alumna promedio';
+      } else if (pro <= 60) {
+        status = 'Alumna con con bajo desempeño';
+      } else if (pro >= 90) {
+        status = 'Alumna sobresaliente';
+      } 
+      for (let i = 0; i < estudiante.length; i++) {
+        let nombres = estudiante[i];
+        // console.log(nombres);
+        result += `<div class="card">
+          <div class = "estudiante">
+        
+          <p>Nombre: ${nombres.nombre}</p>
+          <p>Correo: ${nombres.correo}</p>
+          <p>Turno: ${nombres.turno}</p>
+          <p>Estatus: ${status}</p>
+          <p>Duración del programa: ${percentDuration}</p>
+          </div>
+          </div> `;
+      }
+      container.innerHTML = result;
+      return result;
+    }
+  });
+};
