a1300722d7dea1e95d2755283691057a17951514
Josceline Jaimes <joscejaimes94@gmail.com>
2019-02-06 00:24:33 -0600

    Añadiendo credenciales

---
+++ credentials/credenciales.js
@@ -0,0 +1,12 @@
+
+  const credentials = {
+    apiKey: "AIzaSyCihx2_wwaHazrySwOO0LLmdBCWtfZgoek",
+    authDomain: "login-35451.firebaseapp.com",
+    databaseURL: "https://login-35451.firebaseio.com",
+    projectId: "login-35451",
+    storageBucket: "login-35451.appspot.com",
+    messagingSenderId: "276377229885"
+  };
+ firebase.initializeApp(credentials);
+
+
